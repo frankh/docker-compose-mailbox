9d66ac568c9b2d50110ffc5f92648cb0fb4a362d
Frank Hamand <frankhamand@gmail.com>
2015-03-16 17:40:16 +0000

    add opendkim box

---
+++ opendkim/setup_and_init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ! [[ -f /etc/certs/dkim.private ]]; then
+  echo "DKIM private key not found in data/certs, autogenerating..."
+
+  mkdir -p /tmp/certs
+  opendkim-genkey --testmode --domain=$MAIL_DOMAIN --selector=$MAIL_DKIM_SELECTOR --directory=/tmp/certs
+  mv /tmp/certs/$MAIL_DKIM_SELECTOR.private /etc/certs/dkim.private
+  mv /tmp/certs/$MAIL_DKIM_SELECTOR.txt /etc/certs/dkim.txt
+
+  echo "Generated keys and DNS settings"
+fi
+
+exec /sbin/my_init
