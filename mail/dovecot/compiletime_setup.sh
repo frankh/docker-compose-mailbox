938db93fd20d630a8bdb6c2ebb0de19fd240019a
Frank Hamand <frankhamand@gmail.com>
2015-03-17 12:16:36 +0000

    add MAIL_MX_DOMAIN to settings

--- mail/dovecot/compiletime_setup.sh
+++ mail/dovecot/compiletime_setup.sh
@@ -2,3 +2,4 @@
 . /tmp/settings.env
 
 sed -i -e "s/%MAIL_DOMAIN%/${MAIL_DOMAIN}/g" /etc/postfix/main.cf
+sed -i -e "s/%MAIL_MX_DOMAIN%/${MAIL_MX_DOMAIN}/g" /etc/postfix/main.cf
--- mail/postfix/compiletime_setup.sh
+++ mail/postfix/compiletime_setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 . /tmp/settings.env
 
-sed -i -e "s/%MAIL_DOMAIN%/${MAIL_DOMAIN}/g" /etc/dovecot/conf.d/15-lda.conf
+sed -i -e "s/%MAIL_MX_DOMAIN%/${MAIL_MX_DOMAIN}/g" /etc/dovecot/conf.d/15-lda.conf
