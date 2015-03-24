74d506e03cc6c621ab1001051a8fed53d0945233
Frank Hamand <frankhamand@gmail.com>
2015-03-24 11:03:02 +0000

    add messages to backup scripts

--- storagebackup/mysql_backup.sh
+++ storagebackup/mysql_backup.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-mysqldump -u root -ppassword -h mysql roundcube > /backup/db_roundcube_backup-$(date +"%Y-%m-%d_%H:%M").sql
-mysqldump -u root -ppassword -h mysql vimbadmin > /backup/db_vimbadmin_backup-$(date +"%Y-%m-%d_%H:%M").sql
+echo "Backing up mail databases..."
+echo "Dumping roundcube DB"
+mysqldump -u root -ppassword -h mysql roundcube > /backup/db_roundcube_backup-$(date +"%Y-%m-%d_%H:%M").sql 2> /dev/null
+
+echo "Dumping vimbadmin DB"
+mysqldump -u root -ppassword -h mysql vimbadmin > /backup/db_vimbadmin_backup-$(date +"%Y-%m-%d_%H:%M").sql 2> /dev/null
--- storagebackup/vmail_backup.sh
+++ storagebackup/vmail_backup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+echo "Backing up vmail folders..."
 find /var/vmail/* -depth -empty -type d -exec touch {}/.keep \;
 cd /var/vmail
-git add . --all
-git commit -m 'Backup commit'
+git add . --all &> /dev/null
+git commit -m 'Backup commit' &> /dev/null
+echo "Committed changes - archiving..."
 git archive master | gzip > /backup/vmail_backup-$(date +"%Y-%m-%d_%H:%M").tar.gz
+echo "Done"
