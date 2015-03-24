e516018eb9cb6166bd492e395485783a629b9ff0
Frank Hamand <frankhamand@gmail.com>
2015-03-24 09:38:48 +0000

    split mysql backup to per database

--- storagebackup/mysql_backup.sh
+++ storagebackup/mysql_backup.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-mysqldump -u root -ppassword -h mysql --all-databases > /backup/database_backup-$(date +"%Y-%m-%d_%H:%M").sql
+mysqldump -u root -ppassword -h mysql roundcube > /backup/db_roundcube_backup-$(date +"%Y-%m-%d_%H:%M").sql
+mysqldump -u root -ppassword -h mysql vimbadmin > /backup/db_vimbadmin_backup-$(date +"%Y-%m-%d_%H:%M").sql
