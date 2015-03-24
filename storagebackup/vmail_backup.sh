57585294228c39743f571ed6c6551ef94d0b0e39
Frank Hamand <frankhamand@gmail.com>
2015-03-24 11:09:10 +0000

    test

--- storagebackup/vmail_backup.sh
+++ storagebackup/vmail_backup.sh
@@ -4,8 +4,8 @@ find /var/vmail/* -depth -empty -type d -exec touch {}/.keep \;
 cd /var/vmail
 # Ensure git crashing doesn't break backups
 rm -f /var/vmail/.git/index.lock
-git add . --all &> /dev/null
-git commit -m 'Backup commit' &> /dev/null
+git add . --all
+git commit -m 'Backup commit'
 echo "Committed changes - archiving..."
 git archive master | gzip > /backup/vmail_backup-$(date +"%Y-%m-%d_%H:%M").tar.gz
 echo "Done"
