91705b2d31d9cbdeb08b2ac39b5e77e367a06cc0
Frank Hamand <frankhamand@gmail.com>
2015-03-24 11:05:54 +0000

    fix git crashing breaking backups

--- storagebackup/vmail_backup.sh
+++ storagebackup/vmail_backup.sh
@@ -2,6 +2,8 @@
 echo "Backing up vmail folders..."
 find /var/vmail/* -depth -empty -type d -exec touch {}/.keep \;
 cd /var/vmail
+# Ensure git crashing doesn't break backups
+rm -f /var/vmail/.git/index.lock
 git add . --all &> /dev/null
 git commit -m 'Backup commit' &> /dev/null
 echo "Committed changes - archiving..."
